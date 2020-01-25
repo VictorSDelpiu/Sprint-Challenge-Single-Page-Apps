660d96daf071c1de352e67ec9cba3c22f750dcf2
Victor Sanchez <v08311996@gmail.com>
2020-01-24 21:28:28 -0500

    MVP

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-bottom: 40px;
+background-color: lightblue;
+border: 1.2px solid black;
+color:white;
+font-family: 'Bangers', cursive;
+`;
+
+  return (
+    <StyledDiv>
+      <img src={props.char.image}></img>
+      <p>{props.char.name}</p>
+      <p>{props.char.species}</p>
+      <p>{props.char.gender}</p>
+      <p>{props.char.status}</p>
+    </StyledDiv>
+  );
 }
